333afe3d52b26f6282344b07c58e81996b14def2
Colby Patterson <cpatt@byui.edu>
2022-09-08 09:57:33 -0600

    Get and display expenses from api

--- util/http.js
+++ util/http.js
@@ -4,3 +4,20 @@ import { URL } from "@env";
 export function storeExpense(expenseData) {
   axios.post(URL + "expenses.json", expenseData);
 }
+
+export async function fetchExpenses() {
+  const response = await axios.get(URL + "expenses.json");
+
+  const expenses = [];
+
+  for (const key in response.data) {
+    const expenseObject = {
+      id: key,
+      amount: response.data[key].amount,
+      date: new Date(response.data[key].date),
+      description: response.data[key].description,
+    };
+    expenses.push(expenseObject);
+  }
+  return expenses;
+}
